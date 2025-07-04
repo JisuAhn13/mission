11f9da32e0ab370e74d10baa6788b52c34e346ab
DESKTOP-5FBE16T\User <oegji@naver.com>
2025-07-04 14:22:00 +0900

    mission2 refactoring

--- mission/mission2/m2_test.cpp
+++ mission/mission2/m2_test.cpp
@@ -6,7 +6,11 @@ using namespace testing;
 class m2_UnitTest : public Test {
 public:
 	void SetUp() override {
-		;
+		new_car->registerCarTpye(type);
+		new_car->registerCarEngine(engine);
+		new_car->registerCarBreaksystem(breakSystem);
+		new_car->registerCarSteeringSystem(steeringSystem);
+		new_car->registerCarRunTest(runtest);
 	}
 	int stack[10];
 	car* new_car= new car();
@@ -18,33 +22,28 @@ public:
 };
 
 TEST_F(m2_UnitTest, carTypeTest) {
-	new_car->registerCarTpye(type);
 	EXPECT_EQ(SEDAN, type.selectCarType(stack, SEDAN));
 	EXPECT_EQ(SUV, type.selectCarType(stack, SUV));
 	EXPECT_EQ(TRUCK, type.selectCarType(stack, TRUCK));
 }
 
 TEST_F(m2_UnitTest, carEngineTest) {
-	new_car->registerCarEngine(engine);
 	EXPECT_EQ(GM, engine.selectEngine(stack, GM));
 	EXPECT_EQ(TOYOTA, engine.selectEngine(stack, TOYOTA)); 
 	EXPECT_EQ(WIA, engine.selectEngine(stack, WIA));
 }
 
 TEST_F(m2_UnitTest, carBreakTest) {
-	new_car->registerCarBreaksystem(breakSystem);
 	EXPECT_EQ(MANDO, breakSystem.selectBreakSystem(stack, MANDO));
 	EXPECT_EQ(CONTINENTAL, breakSystem.selectBreakSystem(stack, CONTINENTAL));
 	EXPECT_EQ(BOSCH_B, breakSystem.selectBreakSystem(stack, BOSCH_B));
 }
 
 TEST_F(m2_UnitTest, carsteeringTest) {
-	new_car->registerCarSteeringSystem(steeringSystem);
 	EXPECT_EQ(BOSCH_S, steeringSystem.selectSteeringSystem(stack, BOSCH_S));
 	EXPECT_EQ(MOBIS, steeringSystem.selectSteeringSystem(stack, MOBIS));
 }
 
 TEST_F(m2_UnitTest, carRunTest) {
-	new_car->registerCarRunTest(runtest);
 	runtest.runProducedCar(stack);
 }
